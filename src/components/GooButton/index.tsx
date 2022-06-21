4a22a4cbe82338f3adc15d7a4f2c8b816b79e22d
Sergey Komarov <anaxronik@gmail.com>
2022-06-21 12:06:49 +0500

    add sass

--- src/components/GooButton/index.tsx
+++ src/components/GooButton/index.tsx
@@ -1,20 +1,24 @@
+import "./styles.scss";
+
 import React from "react";
 
 export interface IGooButtonProps {
-    name: string;
-    number: number;
-    boolean: boolean;
+  name: string;
+  number: number;
+  boolean: boolean;
 }
 
 const GooButton: React.FC<IGooButtonProps> = (props) => {
-    return (
-        <div>
-            <div>GooButton</div>
-            <div>props.name: {props.name}</div>
-            <div>props.number: {props.number}</div>
-            <div>props.boolean: {props.boolean ? 'true' : 'false'}</div>
-        </div>
-    );
+  return (
+    <div className="goo-button">
+      <div className="goo-button__1">GooButton</div>
+      <div className="goo-button__2">props.name: {props.name}</div>
+      <div className="goo-button__3">props.number: {props.number}</div>
+      <div className="goo-button__4">
+        props.boolean: {props.boolean ? "true" : "false"}
+      </div>
+    </div>
+  );
 };
 
 export default GooButton;
